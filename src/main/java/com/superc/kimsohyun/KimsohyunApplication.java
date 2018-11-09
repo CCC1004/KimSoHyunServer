bc0f15caed006e81b427f8df9a2f97b81e38c917
CCC1004 <15810586466@163.com>
2018-11-09 10:51:53 +0800

    Initial commit

---
+++ src/main/java/com/superc/kimsohyun/KimsohyunApplication.java
@@ -0,0 +1,12 @@
+package com.superc.kimsohyun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KimsohyunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KimsohyunApplication.class, args);
+    }
+}
