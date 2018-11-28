166579788d880c244d763fb630ac3ecfa4cbaedb
CCC1004 <15810586466@163.com>
2018-11-28 16:45:21 +0800

    20181127

--- web/src/main/java/com/superC/kimsohyun/KimsohyunApplication.java
+++ web/src/main/java/com/superC/kimsohyun/KimsohyunApplication.java
@@ -1,4 +1,4 @@
-package com.superC.kimsohyun;
+package com.superc.kimsohyun;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- web/src/main/java/com/superC/kimsohyun/controller/WXLoginController.java
+++ web/src/main/java/com/superC/kimsohyun/controller/WXLoginController.java
@@ -1,30 +1,66 @@
 package com.superc.kimsohyun.controller;
 
+import com.superc.kimsohyun.common.HttpClientUtil;
+import com.superc.kimsohyun.common.IMoocJSONResult;
+import com.superc.kimsohyun.common.JsonUtils;
+import com.superc.kimsohyun.models.WxSessionModel;
+import com.superc.kimsohyun.properties.WxInfoProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
+ * 微信登录
+ *
  * @Author: superC
  * @Date: 2018/11/9 11:40
  */
 @RestController
-public class WXLoginController {
+@RequestMapping("/wxLogin")
+public class WxLoginController {
+
+    @Autowired
+    private WxInfoProperties wxInfoProperties;
 
     /**
      * 测试是否整合成功
      */
     @RequestMapping(value = "/test")
-    public String testZH(){
+    public String testZH() {
         return "工程整合成功！！！";
     }
 
     /**
      * 微信登录
      */
-    @PostMapping("/wxLogin")
-    public String wxLogin() {
-        return "ok123";
+    @PostMapping(value = "/wxLogin")
+    public IMoocJSONResult wxLogin(@RequestParam("code") String code) {
+
+        System.out.println("wxlogin - code: " + code);
+
+        String url = wxInfoProperties.getUrl();
+        Map<String, String> param = new HashMap<>();
+        param.put("appid", wxInfoProperties.getAppid());
+        param.put("secret", wxInfoProperties.getSecret());
+        param.put("js_code", code);
+        param.put("grant_type", wxInfoProperties.getGrant_type());
+
+        String wxResult = HttpClientUtil.doGet(url, param);
+        System.out.println("json串：  " + wxResult);
+
+//        转换为json对象
+        WxSessionModel model = JsonUtils.jsonToPojo(wxResult, WxSessionModel.class);
+
+        // 存入session到redis
+
+
+        return IMoocJSONResult.ok();
+
     }
 
 }
